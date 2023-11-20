64c745557bdc5759f4e141a0a513628aac74a452
Tanya Bulaeva <t.akberdina@gmail.com>
2023-11-20 16:47:27 +0500

    испр

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
 const http = require('http');
 const port = 3000;
 const getUsers = require('./modules/users');
-
+const hostname = 'http://127.0.0.1'
 
 
 const server = http.createServer((request, response) => {
-    const url = new URL(http.request.url, 'http://127.0.0.1');
+    const url = new URL(request.url, `http://${hostname}`);
+
     const query = url.searchParams;
-    console.log(query)
-    if (query.has('hello')){
-        if (name) {
-            response.status = 200;
-            response.statusMessage = 'OK';
-            response.setHeader('Content-Type', 'text/plain');
-            response.write(`Hello, ${name}`);
-            response.end();
-            return
-        }
-        response.statusCode = 400;
-        response.setHeader('Content-Type', 'text/plain');
-        response.write("Enter a name");
-        response.end();
-        return;
-    } else  if (request.url === '/users'){
-        response.status = 200;
-        response.statusMessage = "OK";
-        response.setHeader('Content-Type', 'application/json');
-        response.write(getUsers());
-        response.end();
-        return
-    }    else if (url.search === "") {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
-        response.write('Hello, World!');
-        response.end();
-        return;
-      } else {
-        response.statusCode = 500;
-        response.end();
-        return;
-      }
+    if (query.has("hello")) {
+    const name = query.get("hello");
 
+    if (name) {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/plain');
+      response.write(`Hello, ${name}!`);
+      response.end();
+      return;
+    }
 
+    response.statusCode = 400;
+    response.setHeader('Content-Type', 'text/plain');
+    response.write("Enter a name");
+    response.end();
+    return;
+  } else if (request.url  === "/users") {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.write(getUsers());
+    response.end();
+    return;
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.write("Hello, World!");
+    response.end();
+  } else {
+    response.statusCode = 500;
+    response.end();
+    return;
+  }
+ });
     // Написать обработчик запроса:
     // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
     // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
@@ -49,7 +48,7 @@ const server = http.createServer((request, response) => {
     // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
     // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
 
-});
+
 
 server.listen(port,  () => {
     console.log(`Сервер запущен по адресу http://127.0.0.1:${port}`);
